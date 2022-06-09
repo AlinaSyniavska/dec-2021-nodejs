26ccba0d2d26e69360aa14cba453d9e09fcc8fb1
AlinaSyniavska <alina22syniavska@gmail.com>
2022-06-09 14:56:12 +0300

    base code

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-// const fs = require('fs');
-const fs = require('fs/promises');
-const path = require('path');
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+
+// app.listen(3000) //port 3000 - default React
+app.listen(5000)
 
 
