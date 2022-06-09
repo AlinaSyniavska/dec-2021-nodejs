a64bf723f600cbe94596524c117eb3495603c72d
AlinaSyniavska <alina22syniavska@gmail.com>
2022-06-10 00:34:31 +0300

    add express

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-const fs = require('fs/promises');
-const path = require('path');
+const express = require('express');
+
+const app = express();
+
+
+app.listen(5000, () => {
+    console.log('Started on port 5000')
+})
+
+
 
 
