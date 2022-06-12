10b327998365eaeb4737b79d35caba1aa85ea809
AlinaSyniavska <alina22syniavska@gmail.com>
2022-06-13 00:23:52 +0300

    lecture code

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const {fileService} = require('./services');
 
 const app = express();
 
+// fileService.append('./dataBase/data.txt', 'Append some data to data.txt. ').then();
+// fileService.append('./dataBase/data2.txt', 'Append some data2 to data2.txt. ').then();
+
+// fileService.readDir('./dataBase');
+
+fileService.readDataStream('./dataBase/data3.txt').then();
 
 app.listen(5000, () => {
     console.log('Started on port 5000')
--- services/file.service.js
+++ services/file.service.js
@@ -1,6 +1,49 @@
 const fs = require('fs/promises');
 const path = require('path');
+const { open } = require('node:fs/promises');
+
 
 module.exports = {
+    append: async (path, data) => {
+        try {
+            await fs.appendFile(path, data)
+        } catch (e) {
+            console.log(e);
+        }
+    },
 
+    readDir: async (path) => {
+        try {
+            const files = await fs.readdir(path);
+
+            for (const file of files) {
+                console.log(file);
+
+                const data = await fs.readFile(`${path}/${file}`);
+                console.log(data);
+                console.log(data.toString());
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    },
+
+    readDataStream: async (path) => {
+        try {
+            const fd = await open(path);
+            const readStream = fd.createReadStream();
+
+            readStream.on('data', (chunk) => {
+                console.log('chunk');
+                console.log(chunk);
+            });
+
+            readStream.on('end', () => {
+                console.log('File DONE!');
+            })
+        } catch (e) {
+            console.error('ERROR READ_DATA_STREAM');
+            console.log(e);
+        }
+    },
 }
