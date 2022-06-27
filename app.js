e8496f9c49048053804f880b2d9d005534b9bc20
AlinaSyniavska <alina22syniavska@gmail.com>
2022-06-27 23:57:32 +0300

    add cors()

--- app.js
+++ app.js
@@ -1,36 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
-const {config} = require("./configs");
-const {userRouter, authRouter} = require("./routes");
+const { config } = require('./configs');
+const { userRouter, authRouter } = require('./routes');
 
 mongoose.connect(config.MONGO_URL);
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/users', userRouter);
 app.use('/auth', authRouter);
 
-app.use('*', ((req, res) => {
-    res.status(404).json('Route not found');
-}));
+app.use('*', (req, res) => {
+  res.status(404).json('Route not found');
+});
 
 app.use((err, req, res, next) => {
-    res
-        .status(err.status || 400)
-        .json({
-            error: err.message || 'Unknown Error',
-            code: err.status || 400,
-        });
+  res.status(err.status || 400).json({
+    error: err.message || 'Unknown Error',
+    code: err.status || 400,
+  });
 });
 
 app.listen(config.PORT, () => {
-    console.log(`Started on port ${config.PORT}`);
-})
-
-
-
-
+  console.log(`Started on port ${config.PORT}`);
+});
