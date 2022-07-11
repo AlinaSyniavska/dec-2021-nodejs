e9143891c647c8c55a9f00700fee9ddc3a8237f6
AlinaSyniavska <alina22syniavska@gmail.com>
2022-07-11 09:18:14 +0300

    fix _configureCors

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
-const {config} = require('./configs');
+const { config } = require('./configs');
 
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const {userRouter, authRouter} = require('./routes');
+const { userRouter, authRouter } = require('./routes');
 const cronRun = require('./cron');
 
 mongoose.connect(config.MONGO_URL);
@@ -13,45 +13,52 @@ mongoose.connect(config.MONGO_URL);
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 if (config.MODE !== 'prod') {
-    const morgan = require('morgan');
-    app.use(morgan('dev'));
+  const morgan = require('morgan');
+  app.use(morgan('dev'));
 }
 
 // app.use(cors(_configureCors()));
+app.use(cors());
 app.use('/users', userRouter);
-app.use('/auth', cors(_configureCors()), authRouter);
+app.use('/auth', authRouter);
+// app.use('/auth', cors(_configureCors()), authRouter);
 
 app.use('*', (req, res) => {
-    res.status(404).json('Route not found');
+  res.status(404).json('Route not found');
 });
 
 app.use((err, req, res, next) => {
-    res.status(err.status || 400).json({
-        error: err.message || 'Unknown Error',
-        code: err.status || 400,
-    });
+  res.status(err.status || 400).json({
+    error: err.message || 'Unknown Error',
+    code: err.status || 400,
+  });
 });
 
 app.listen(config.PORT, () => {
-    console.log(`Started on port ${config.PORT}`);
+  console.log(`Started on port ${config.PORT}`);
 
-    cronRun();
+  cronRun();
 });
 
 function _configureCors() {
-    const whitelist = config.CORS_WHITE_LIST.split(';');
+  const whitelist = config.CORS_WHITE_LIST.split(';');
 
-    return {
-        origin: (origin, callback) => {
-            if (whitelist.includes(origin)) {
-                return callback(null, true);
-            }
+  console.log(whitelist);
 
-            callback(new Error('Not allowed by CORS'));
-        }
-    };
+  return {
+    origin: (origin, callback) => {
+      console.log(origin);
+
+      console.log(whitelist.includes(origin));
+
+      if (whitelist.includes(origin) || !origin) {
+        return callback(null, true);
+      }
+
+      callback(new Error('Not allowed by CORS'));
+    },
+  };
 }
-
--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,126 +1,131 @@
-const {tokenService, passwordService, emailService} = require("../services");
-const {userPresenter} = require("../presenters");
-const {OAuth, ActionToken, User} = require("../dataBase");
-const {emailActionEnum} = require("../enums");
+const { tokenService, passwordService, emailService } = require('../services');
+const { userPresenter } = require('../presenters');
+const { OAuth, ActionToken, User } = require('../dataBase');
+const { emailActionEnum } = require('../enums');
 
 module.exports = {
-    login: async (req, res, next) => {
-        try {
-            const {password: hashPassword, _id, name} = req.user;
-            const {password, email} = req.body;
+  login: async (req, res, next) => {
+    try {
+      const { password: hashPassword, _id, name } = req.user;
+      const { password, email } = req.body;
 
-            await emailService.sendMail('alina22syniavska@gmail.com', emailActionEnum.WELCOME, {userName: name});
-            // await emailService.sendMail(email, emailActionEnum.WELCOME, {userName: name});
+      // await emailService.sendMail('alina22syniavska@gmail.com', emailActionEnum.WELCOME, {userName: name});
+      await emailService.sendMail(email, emailActionEnum.WELCOME, { userName: name });
 
-            await passwordService.comparePassword(hashPassword, password);
-            // await req.user.comparePasswords(password); //UserSchema.methods
+      await passwordService.comparePassword(hashPassword, password);
+      // await req.user.comparePasswords(password); //UserSchema.methods
 
-            const tokens = tokenService.generateAuthTokens();
+      const tokens = tokenService.generateAuthTokens();
 
-            const userForResponse = userPresenter.userResponse(req.user);
+      const userForResponse = userPresenter.userResponse(req.user);
 
-            await OAuth.create({
-                userId: _id,
-                ...tokens
-            });
+      await OAuth.create({
+        userId: _id,
+        ...tokens,
+      });
 
-            res.json({
-                user: userForResponse,
-                ...tokens
-            });
-        } catch (e) {
-            next(e);
-        }
-    },
+      res.json({
+        user: userForResponse,
+        ...tokens,
+      });
+    } catch (e) {
+      next(e);
+    }
+  },
 
-    refreshToken: async (req, res, next) => {
-        try {
-            const {userId, refresh_token} = req.tokenInfo;
+  refreshToken: async (req, res, next) => {
+    try {
+      const { userId, refresh_token } = req.tokenInfo;
 
-            await OAuth.deleteOne({refresh_token});
+      await OAuth.deleteOne({ refresh_token });
 
-            const tokens = tokenService.generateAuthTokens();
+      const tokens = tokenService.generateAuthTokens();
 
-            await OAuth.create({
-                userId,
-                ...tokens
-            });
+      await OAuth.create({
+        userId,
+        ...tokens,
+      });
 
-            res.json(tokens);
-        } catch (e) {
-            next(e);
-        }
-    },
+      res.json(tokens);
+    } catch (e) {
+      next(e);
+    }
+  },
 
-    logout: async (req, res, next) => {
-        try {
-            const {access_token, user} = req;
-            const {email, name} = user;
+  logout: async (req, res, next) => {
+    try {
+      const { access_token, user } = req;
+      const { email, name } = user;
 
-            await OAuth.deleteOne({access_token});
+      await OAuth.deleteOne({ access_token });
 
-            await emailService.sendMailHbs(email, emailActionEnum.LOGOUT, {name, count: 1});
+      await emailService.sendMailHbs(email, emailActionEnum.LOGOUT, { name, count: 1 });
 
-            res.sendStatus(204);
-        } catch (e) {
-            next(e);
-        }
-    },
+      res.sendStatus(204);
+    } catch (e) {
+      next(e);
+    }
+  },
 
-    logoutAllDevices: async (req, res, next) => {
-        try {
-            const {_id, email, name} = req.user;
+  logoutAllDevices: async (req, res, next) => {
+    try {
+      const { _id, email, name } = req.user;
 
-            const {deletedCount} = await OAuth.deleteMany({userId: _id});
+      const { deletedCount } = await OAuth.deleteMany({ userId: _id });
 
-            await emailService.sendMailHbs(email, emailActionEnum.LOGOUT, {name, count: deletedCount});
+      await emailService.sendMailHbs(email, emailActionEnum.LOGOUT, { name, count: deletedCount });
 
-            res.sendStatus(204);
-        } catch (e) {
-            next(e);
-        }
-    },
+      res.sendStatus(204);
+    } catch (e) {
+      next(e);
+    }
+  },
 
-    forgotPassword: async (req, res, next) => {
-        try {
-            const {_id, name, email} = req.user;
-            const token = tokenService.generateActionToken(emailActionEnum.FORGOT_PASSWORD, {name, _id});
+  forgotPassword: async (req, res, next) => {
+    try {
+      const { _id, name, email } = req.user;
+      const token = tokenService.generateActionToken(emailActionEnum.FORGOT_PASSWORD, {
+        name,
+        _id,
+      });
 
-            await ActionToken.create({
-                    userId: _id,
-                    token,
-                    actionType: emailActionEnum.FORGOT_PASSWORD
-                }
-            );
+      await ActionToken.create({
+        userId: _id,
+        token,
+        actionType: emailActionEnum.FORGOT_PASSWORD,
+      });
 
-            await emailService.sendMailHbs(email, emailActionEnum.FORGOT_PASSWORD, {name});
+      await emailService.sendMailHbs(email, emailActionEnum.FORGOT_PASSWORD, { name });
 
-            // await emailService.sendMail(email, emailActionEnum.FORGOT_PASSWORD, {userName: name, token}); //real code
-            await emailService.sendMail(
-                'alina22syniavska@gmail.com',
-                emailActionEnum.FORGOT_PASSWORD,
-                {userName: name, token}
-            ); //test code
+      // await emailService.sendMail(email, emailActionEnum.FORGOT_PASSWORD, {userName: name, token}); //real code
+      await emailService.sendMail('alina22syniavska@gmail.com', emailActionEnum.FORGOT_PASSWORD, {
+        userName: name,
+        token,
+      }); //test code
 
-            res.json('Ok');
-        } catch (e) {
-            next(e);
-        }
-    },
+      res.json('Ok');
+    } catch (e) {
+      next(e);
+    }
+  },
 
-    setForgotPassword: async (req, res, next) => {
-        try{
-            const {_id} = req.user;
-            const {password} = req.body;
+  setForgotPassword: async (req, res, next) => {
+    try {
+      const { _id } = req.user;
+      const { password } = req.body;
 
-            const hashedPassword = await passwordService.hashPassword(password);
+      const hashedPassword = await passwordService.hashPassword(password);
 
-            const  updatedUser = await User.findByIdAndUpdate(_id, {password: hashedPassword}, {new: true});
-            await ActionToken.deleteOne({actionType: emailActionEnum.FORGOT_PASSWORD, userId: _id});
+      const updatedUser = await User.findByIdAndUpdate(
+        _id,
+        { password: hashedPassword },
+        { new: true }
+      );
+      await ActionToken.deleteOne({ actionType: emailActionEnum.FORGOT_PASSWORD, userId: _id });
 
-            res.json(updatedUser);
-        } catch (e) {
-            next(e);
-        }
-    },
+      res.json(updatedUser);
+    } catch (e) {
+      next(e);
+    }
+  },
 };
