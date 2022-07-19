d8cf7edc308768e5fc9e9dc6be3abe1d7f4c124d
AlinaSyniavska <alina22syniavska@gmail.com>
2022-07-19 06:28:24 +0300

    add prop phone into userValidator

--- validators/user.validator.js
+++ validators/user.validator.js
@@ -1,19 +1,19 @@
 const Joi = require('joi');
 
-const {emailValidator, passwordValidator, phoneValidator} = require("./common.validator");
+const { emailValidator, passwordValidator, phoneValidator } = require('./common.validator');
 
 module.exports = {
-    newUserValidator: Joi.object({
-        name: Joi.string().alphanum().trim(true).min(2).max(100).required(),
-        age: Joi.number().integer().min(1).max(120),
-        email: emailValidator.required(),
-        password: passwordValidator.required(),
-        phone: phoneValidator.required(),
-    }),
+  newUserValidator: Joi.object({
+    name: Joi.string().alphanum().trim(true).min(2).max(100).required(),
+    age: Joi.number().integer().min(1).max(120),
+    email: emailValidator.required(),
+    password: passwordValidator.required(),
+    phone: phoneValidator.required(),
+  }),
 
-    updateUserValidator: Joi.object({
-        name: Joi.string().alphanum().trim(true).min(2).max(100),
-        age: Joi.number().integer().min(1).max(120),
-    }),
+  updateUserValidator: Joi.object({
+    name: Joi.string().alphanum().trim(true).min(2).max(100),
+    age: Joi.number().integer().min(1).max(120),
+    phone: phoneValidator,
+  }),
 };
-
