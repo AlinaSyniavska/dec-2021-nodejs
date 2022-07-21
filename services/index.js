389f57fd1e9a2e6d8f64f149889dbea7d8219d59
AlinaSyniavska <alina22syniavska@gmail.com>
2022-07-21 21:58:44 +0300

    add /services/index.js

--- services/index.js
+++ services/index.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+    userService: require('./user.service'),
+    passwordService: require('./password.service'),
+    tokenService: require('./token.service'),
+    emailService: require('./email.service'),
+};
